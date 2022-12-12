984c91e7b908806649c09de3e960615e0e2d274b
Любаев Павел Александрович <p.lyubaev@sigma-it.ru>
2022-12-12 11:57:51 +0500

    Db mariadb

--- mysql-container.sh
+++
@@ -1,7 +0,0 @@
-docker run --name mysql -d -p3306:3306 \
-  --restart=always \
-  -e MYSQL_ROOT_PASSWORD=RootPassword \
-  -e MYSQL_DATABASE=iot-server \
-  -e MYSQL_USER=iot-server \
-  -e MYSQL_PASSWORD=iot-server-password \
-  docker-proxy.cloud.ga66a.ru/mysql:8.0.31
